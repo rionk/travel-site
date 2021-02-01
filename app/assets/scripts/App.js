a650fa4f6546884d33896fdc9edc070b830f64af
rion <rionwolfz@daum.net>
2021-02-01 11:46:42 +0900

    complete reveal-scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import "../scss/main.scss";
 import MobileMenu from "./modules/MobileMenu.js";
+import RevealOnScroll from "./modules/RevealOnScroll.js";
 
 new MobileMenu();
+new RevealOnScroll(".feature-item");
 
 if(import.meta.webpackHot) {
     import.meta.webpackHot.accept();
